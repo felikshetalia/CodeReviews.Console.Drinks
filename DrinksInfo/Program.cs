e85db550a5c010df4fbf9965eed9975563d866d1
felikshetalia <mccorsica@yahoo.com>
2026-07-28 10:54:58 +0200

    initial commit after setup

---
+++ DrinksInfo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+namespace CodeReviews.Console.Drinks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = Setup();
+
+        string baseUrl =
+            config["DrinksDB:BaseUrl"]
+            ?? throw new InvalidOperationException(
+                "DrinksDB:BaseUrl is missing.");
+
+        string apiKey =
+            config["DrinksDB:ApiKey"]
+            ?? throw new InvalidOperationException(
+                "DrinksDB:ApiKey is missing.");
+    }
+
+    private static IConfigurationRoot Setup()
+    {
+        IConfigurationRoot config =
+            new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(
+                    "Configuration/appsettings.json",
+                    optional: false)
+                .Build();
+
+        return config;
+    }
+}
